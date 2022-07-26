d5e7dd2e949b0f66047e72a42f5a48c0d9bbfff7
Aleksey Gavrikov <a.e.gavrikoff@gmail.com>
2022-07-26 19:36:06 +0300

    feat: added IProduct to Product comp

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Product from "./components/Product";
+import { products } from "./data/products";
+
+const App = () => {
+  return (
+    <div className="container mx-auto max-w-2xl pt-5 ">
+      <Product product={products[0]} />
+    </div>
+  );
+};
+
+export default App;
