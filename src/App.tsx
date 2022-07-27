97d716fddac59c80fb98bac8e00f6f4ec9277193
Aleksey Gavrikov <a.e.gavrikoff@gmail.com>
2022-07-27 09:36:02 +0300

    feat: state for the loading error

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,27 @@
 import Product from "./components/Product";
 import { IProduct } from "./models";
 import { useEffect, useState } from "react";
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 
 const App = () => {
   const [products, setProducts] = useState<IProduct[]>([]);
-
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
 
   async function fetchProducts() {
-    setLoading(true);
-    const response = await axios.get<IProduct[]>(
-      "https://fakestoreapi.com/products?limit=5"
-    );
-    setProducts(response.data);
-    setLoading(false);
+    try {
+      setError("");
+      setLoading(true);
+      const response = await axios.get<IProduct[]>(
+        "https://fakestoreapi.com/roducts?limit=5"
+      );
+      setProducts(response.data);
+      setLoading(false);
+    } catch (e: unknown) {
+      const error = e as AxiosError;
+      setLoading(false);
+      setError(error.message);
+    }
   }
 
   useEffect(() => {
@@ -24,6 +31,7 @@ const App = () => {
   return (
     <div className="container mx-auto max-w-2xl pt-5 ">
       {loading && <p className="text-center">Loading...</p>}
+      {error && <p className="text-center text-red-600">It seems something went wrong:<br/> {error}</p>}
       {products.map((product) => (
         <Product product={product} key={product.id} />
       ))}
