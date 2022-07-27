c2f2909c48be84b231a286830128ef10bd5b7a8c
Aleksey Gavrikov <a.e.gavrikoff@gmail.com>
2022-07-27 09:15:30 +0300

    feat: product data is fetched from the API

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import Product from "./components/Product";
-import { products } from "./data/products";
+import { IProduct } from "./models";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const App = () => {
+  const [products, setProducts] = useState<IProduct[]>([]);
+
+  async function fetchProducts() {
+    const response = await axios.get<IProduct[]>(
+      "https://fakestoreapi.com/products?limit=5"
+    );
+    setProducts(response.data);
+  }
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
 
   return (
     <div className="container mx-auto max-w-2xl pt-5 ">
