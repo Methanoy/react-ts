f495c3f89aa7ec80d0fc09fad6d0b81d2e841053
Aleksey Gavrikov <a.e.gavrikoff@gmail.com>
2022-07-27 09:02:30 +0300

    feat: product cards and styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,12 @@ import Product from "./components/Product";
 import { products } from "./data/products";
 
 const App = () => {
+
   return (
     <div className="container mx-auto max-w-2xl pt-5 ">
-      <Product product={products[0]} />
+      {products.map((product) => (
+        <Product product={product} key={product.id} />
+      ))}
     </div>
   );
 };
