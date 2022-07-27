71a3d616fb22488bd8952de3c112f441e5487b9b
Aleksey Gavrikov <a.e.gavrikoff@gmail.com>
2022-07-27 16:21:32 +0300

    feat: add nav with about&product pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,17 @@
-import Product from "./components/Product";
-import { useProducts } from "./hooks/products";
-import { Loader } from "./components/Loader";
-import { ErrorMessage } from "./components/ErrorMessage";
-import { Modal } from "./components/Modal";
-import { CreateProduct } from "./components/CreateProduct";
-import { useContext } from "react";
-import { IProduct } from "./models";
-import { ModalContext } from "./context/ModalContext";
+import { ProductsPage } from "../src/pages/ProductsPage";
+import { AboutPage } from "./pages/AboutPage";
+import { Routes, Route } from "react-router-dom";
+import { Navigation } from "./components/Navigation";
 
 const App = () => {
-  const { products, error, loading, addProduct } = useProducts();
-  const {isModalOpen, openModal, closeModal} = useContext(ModalContext);
-
-  const handleCreateProduct = (product: IProduct) => {
-    closeModal();
-    addProduct(product);
-  };
-
   return (
-    <div className="container mx-auto max-w-2xl pt-5 ">
-      {loading && <Loader />}
-      {error && <ErrorMessage error={error} />}
-      {products.map((product) => (
-        <Product product={product} key={product.id} />
-      ))}
-      {isModalOpen && (
-        <Modal title="Create new product" onClose={closeModal}>
-          <CreateProduct onCreate={handleCreateProduct} />
-        </Modal>
-      )}
-      <button
-        onClick={openModal}
-        className="fixed bottom-5 right-5 rounded-full bg-red-700 text-white text-2xl px-4 py-2"
-      >
-        +
-      </button>
-    </div>
+    <>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<ProductsPage />} />
+        <Route path="/about" element={<AboutPage />} />
+      </Routes>
+    </>
   );
 };
 
---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,6 @@
+
+export const AboutPage = () => {
+return (
+    <p className="text-2xl text-center mb-2">Hello! This page was created just for practice with ReactRouter</p>
+    )
+};
